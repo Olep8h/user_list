8d83e5e0418cec969632735d04a4fc1542cf7bd3
Olep8h <panov@student.vspj.cz>
2022-11-26 22:21:43 +0200

    renamed

---
+++ src/components/Success.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Success = ({ count }) => {
+    return (
+        <div class="success-block">
+            <img src="/assets/success.svg" alt="Success" />
+            <h3>Successful!</h3>
+            <p>Every {count} users been sent an invitation.</p>
+            <button onClick={() => window.location.reload()} className="send-invite-btn">Back</button>
+        </div>
+    );
+};
